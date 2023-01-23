40ebfd74fd409f5f128f9d423950b002246f4350
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-01-23 16:10:34 +0000

    tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,13 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    elements_printed = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end=' ')
-            elements_printed += 1
-        print()
-    except:
-        pass
+    i = 0
+    printed = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            continue
+    print()
     return elements_printed
