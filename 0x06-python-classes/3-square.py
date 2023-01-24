f32733a433734d2c8dcf2d055b12c3cd17edd46f
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-01-24 23:36:28 +0000

    tasks

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""class Square"""
-
-
 class Square:
-    """square class with it's size and proper validation"""
-
     def __init__(self, size=0):
-        if (type(size) is not int):
-            raise TypeError("size must be an integer")
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
         else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+            self.__size = size
 
     def area(self):
-        """calculates and returns area of a square"""
-
-        return self.__size ** 2 
+        size = self.__size
+        return size * size
