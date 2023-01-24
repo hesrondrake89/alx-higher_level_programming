281206d953a4ed027e21b2d472ec2ded30335ae2
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-01-24 23:10:21 +0000

    tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""Defining a class Square."""
+"""  defining a class square alx task """
 
 
 class Square:
+    """Class representing a square"""
+
     def __init__(self, size):
+        """ initialize the class
+        Args:
+           size (int): size of the square
+        """
         self.__size = size
