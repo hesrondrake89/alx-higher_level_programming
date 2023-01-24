c2c4b32129ed9e1e1126d6fa2e9eda9a773e66a7
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-01-24 23:41:02 +0000

    tasks

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""class Square that defines a square"""
+
 
 class Square:
+    """square class with it's size and proper validation"""
+
     def __init__(self, size=0):
         self.__size = size
 
-    # Property
     @property
     def size(self):
         return self.__size
 
-    # Setter modifies
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         return self.__size ** 2
