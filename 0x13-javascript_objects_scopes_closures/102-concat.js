4dfc2233fc3010ecd90ac3343677e57cb9acbabd
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-13 18:43:31 +0000

    tasks

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+async function concatenateFiles(file1, file2, outputFile) {
+  const fArg = await fs.readFile(file1, 'utf-8');
+  const sArg = await fs.readFile(file2, 'utf-8');
+  await fs.writeFile(outputFile, fArg + sArg);
+}
+
+concatenateFiles(process.argv[2], process.argv[3], process.argv[4])
+  .then(() => console.log('Files concatenated successfully!'))
+  .catch((error) => console.error(`Error concatenating files: ${error.message}`));
