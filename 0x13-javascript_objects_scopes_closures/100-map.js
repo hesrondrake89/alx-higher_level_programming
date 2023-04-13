bbeb1f27f3a7d8c9087c23e143e7b966c08bd419
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-13 18:35:40 +0000

    tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
-});
-
-consoe.log(list);
-console.log(newList);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
