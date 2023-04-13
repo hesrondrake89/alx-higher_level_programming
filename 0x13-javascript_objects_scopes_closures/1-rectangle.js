c3a6ff3d87dd24d65c7a55316811880424cf7e2e
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-13 18:01:40 +0000

    tasks

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
   }
 };
