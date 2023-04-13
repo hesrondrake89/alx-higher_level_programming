57c6fb857a7fea8cbe1bbcf0f934ad1040dd0006
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-13 17:50:45 +0000

    tasks

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-    this.width = w;
-    this.height = h;
+  constructor(weight, height) {
+    if ((w > 0) && (h > 0)) {
+    this.width = weight;
+    this.height = height;
+    }
   }
 }
 
