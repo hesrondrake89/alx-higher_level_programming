caa0c15a61237f08fbfa771465b5c1e2ed157adb
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-13 18:22:58 +0000

    tasks

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,31 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
 
 class Square extends Rectangle {
-  constructor (size) {
+  constructor(size) {
     super(size, size);
   }
 }
