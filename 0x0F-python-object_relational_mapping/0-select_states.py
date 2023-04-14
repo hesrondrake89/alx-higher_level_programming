36258b4c9402c00e40dbb69ed96add8a085dd715
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-14 10:42:42 +0000

    tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa
+List all states from the database hbtn_0e_0_usa
 """
 import argparse
 import MySQLdb
