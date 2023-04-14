b401a5397ce67aa8dbefa7d817215db025051e55
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-14 11:49:48 +0000

    tasks

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State objects from the database hbtn_0e_6_usa
+Retrieves all State objects from within the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
