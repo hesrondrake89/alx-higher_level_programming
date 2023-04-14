fa362e5e5bb4ca8be9e62045db4b35fedcfca3d1
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-14 11:50:48 +0000

    tasks

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State objects that contain the letter a from the database
+Prints all State objects that contain the letter a from the database
 """
 import sys
 from model_state import Base, State
