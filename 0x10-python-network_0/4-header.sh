4a88782dcd961025b68783f945d00af61364774b
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-27 17:01:42 +0000

    tasks

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
+# GET request to a given URL with a specific header variable, you can specify the header using the appropriate curl option while sending the request.
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
