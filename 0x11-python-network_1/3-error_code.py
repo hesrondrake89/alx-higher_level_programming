e378d0949284af7e57c5900686dd1d7354e17b38
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-02 15:25:38 +0000

    tasks

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL
-- displays the body of the response (decoded in utf-8).
 """
-
+This script takes a URL as input, sends a request to the URL, and displays the response 
+body decoded in UTF-8.
+"""
 
 if __name__ == "__main__":
     import sys
