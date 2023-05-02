6cf124afc2f0e34413fc3b7f1cc44542f7f5b2d9
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-02 15:49:10 +0000

    tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This script fetches the https://alx-intranet.hbtn.io/status URL using the urllib Python package.
-It then reads the content of the response and prints out its type, content, and UTF-8 decoded content.
+This script fetches 
+- the https://alx-intranet.hbtn.io/status URL using the urllib Python package.
+- It then reads the content of the response and prints out its type, content, and UTF-8 decoded content.
 """
 
 
