efb06a2874dba475ad9614ec209540a5103c2e0f
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-02 15:24:15 +0000

    tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL
-- sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
+"""
+This script takes a URL as input, sends a POST request to the URL, takes an email address as
+a parameter, and displays the response body.
 """
 import sys
 import urllib.parse
