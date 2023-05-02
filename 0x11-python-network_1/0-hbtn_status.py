805e41c8379b6e9efef1fae27b9b4891afdfa81a
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-02 15:21:47 +0000

    tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+"""
+This script fetches the https://alx-intranet.hbtn.io/status URL using the urllib Python package.
+It then reads the content of the response and prints out its type, content, and UTF-8 decoded content.
 """
 
 
-if __name__ == '__main__':
-    import urllib.request
+if name == 'main':
+import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    content = response.read()
+    print("Response Body:")
+    print("\t- Type: {}".format(type(content)))
+    print("\t- Content: {}".format(content))
+    print("\t- Decoded Content (UTF-8): {}".format(content.decode('utf-8')))
