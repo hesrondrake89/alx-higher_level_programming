a25f0228890b8409ce839bffa4d38b134d943508
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-23 21:36:59 +0000

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
