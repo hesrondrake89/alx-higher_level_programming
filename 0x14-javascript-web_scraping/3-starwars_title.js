707935ea231a13188687bc3119513667b783e65e
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-23 21:32:28 +0000

    tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,6 @@
 const request = require('request');
 const filmId = process.argv[2];
 const url = `http://swapi.co/api/films/${filmId}`;
-
 request(url, (error, response, body) => {
   console.log(error || JSON.parse(body).title);
 });
