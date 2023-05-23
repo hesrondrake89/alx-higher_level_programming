3f09452b7e7a4657c547e4bf44d365a331eaf143
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-23 21:41:01 +0000

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
+
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    const films = JSON.parse(body).results;
+    const moviesWithWedge = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+    console.log(`Number of movies with Wedge Antilles: ${moviesWithWedge.length}`);
+  }
 });
